8a293ca091f6af82af5c5c80f7cb442b249aba76
anis6273 <121841936+anis6273@users.noreply.github.com>
2023-01-03 23:25:27 -0300

    Creación de banner

--- IntegradorPortfolio_LopezFontanaAnaCarla/src/app/app.module.ts
+++ IntegradorPortfolio_LopezFontanaAnaCarla/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './comonents/header/header.component';
+import { LogoAPComponent } from './comonents/logo-ap/logo-ap.component';
+import { SocialComponent } from './comonents/social/social.component';
+import { BannerComponent } from './comonents/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
